aa44b70abc766d0397f99301cdc5e0fc46c9afda
Eudis Alves <deleudis@gmail.com>
2023-03-09 20:09:45 -0300

    adicionando componente rede social

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,17 @@
-import Avatar from '../img/Eudis.jpg'
-import '../styles/components/sidebar.sass'
+import SocialNet from './SocialNet';
 
+
+import Avatar from '../img/Eudis.jpg';
+
+import '../styles/components/sidebar.sass';
 
 
 export default function Sidebar() {
     return <aside id="sidebar">
         <img src={Avatar} alt="Eudis Alves"/>
         <p className="title">Desenvolvedor</p>
-        <p>rede social</p>
+        <SocialNet />
         <p>Informações de contatos</p>
-        <a href="" className="btn">Download Currículo</a>
+        <a href="#" className="btn">Download Currículo</a>
     </aside>
 }
--- src/components/SocialNet.jsx
+++ src/components/SocialNet.jsx
@@ -1,7 +1,21 @@
+import { FaLinkedinIN, FaGithub } from 'react-icons/fa'
+import "../styles/components/socialnet.sass"
 
+const socialNet = [
+    { name:"linkedin", icon: <FaLinkedinIN /> },
+    { name: "github", icon: <FaGithub /> },
+];
 
-
-
-export default function SocialNet() {
-    return <div>Redes sociais</div>
+const SocialNet = () => {
+    return (
+        <section id="social-net">
+            {socialNet.map((network) => (
+                <a href="#" className="social-btn" id={network.name} key={network.name}>
+                    {network.icon}
+                </a>
+            ))}
+        </section>
+    )
 }
+
+export default SocialNet;
