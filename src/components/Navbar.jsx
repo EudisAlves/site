3b538fde6f39b6f842c6fe6b03aed7293744a839
Eudis Alves <deleudis@gmail.com>
2024-08-19 23:33:21 -0300

    .fazendo testes de cores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@
 export default function Navbar () {
   return (
     <div className="bg-indigo-500 flex items-center p-10 justify-between lg:flex-row">
-      <div className="text font-serif">
-        <a href="" className="text-white font-mono text-3x1"></a>
-      </div>
       <div>
-        <a href="" className="px-4 text-3x1 bg-yellow-450">Meus projetos</a>
-        <a href="" className="px-4">Linkedin</a>
-        <a href="" className="px-4">Sobre</a>
+        <h1 className="text-3x1 font-serif">Eudis Alves</h1>
+      </div>
+      <div className="space-x-4">
+        <a href="" className="text-gray-100 hover:bg-slate-100">Meus projetos</a>
+        <a href="" className="text-gray-100 hover:bg-slate-100">Linkedin</a>
+        <a href="" className="text-gray-100 hover:bg-slate-100">Sobre</a>
       </div>
     </div>
   )
