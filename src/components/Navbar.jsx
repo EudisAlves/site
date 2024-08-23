c1226f097bf54d2e1039fb5d14e0a50f74de0cb3
Eudis Alves <deleudis@gmail.com>
2024-08-23 19:42:02 -0300

    Adicionando icones na navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import {AiOutLineClose} from 'react-icons/ai'
-import {CgNametag} from 'react-icons/cg'
+import {CiTrophy} from 'react-icons/ci';
+import { AiOutlineContainer } from "react-icons/ai";
 
-export default function Navbar () {
+export default function Navbar() {
   return (
     <div className="bg-indigo-500 flex items-center p-10 justify-between lg:flex-row">
       <div>
-        <h1 className="text-3x1 font-serif"><CgNametag/>Eudis Alves</h1>
+        <a href='#' className="text-white text-3x1 font-serif tracking-wider flex items-center"><CiTrophy />Eudis Alves</a>
       </div>
       <div className="space-x-4">
         <div className="ssm:hidden lg:block space-x-2">
@@ -14,7 +14,7 @@ export default function Navbar () {
           <a href="" className="text-gray-100 hover:bg-slate-100">Sobre</a>
         </div>
         <div>
-          <AiOutLineClose/>
+          <AiOutlineContainer />
         </div>
       </div>
     </div>
