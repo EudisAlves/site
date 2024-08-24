a2a3f7326b68a09ef561c4458e021b7564e604af
Eudis Alves <deleudis@gmail.com>
2024-08-23 21:01:33 -0300

    adicionando cores e trocando icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { CiTrophy } from 'react-icons/ci';
-import { AiOutlineContainer } from "react-icons/ai";
-import { HiMenuAlt1 } from 'react-icons/hi'
+import { GoArrowRight } from "react-icons/go";
+import { GoArrowLeft } from "react-icons/go";
 import { useState } from 'react';
 
 export default function Navbar() {
@@ -18,22 +18,22 @@ export default function Navbar() {
   return (
     <div className="bg-indigo-500 flex items-center p-10 justify-between lg:flex-row">
       <div>
-        <a href='#' className="text-white text-3x1 font-serif tracking-wider flex items-center"><CiTrophy size={30} className='text-yellow mr-2' />Eudis Alves</a>
+        <a href='#' className="text-white text-3x1 font-serif tracking-wider flex items-center"><CiTrophy size={30} className='text-yellow-800 mr-3' />Eudis Alves</a>
       </div>
       <div className="space-x-4">
 
         <div className="ssm:hidden lg:block space-x-2">
 
-          <a href="" className="text-gray-100 hover:bg-slate-100">Meus projetos</a>
-          <a href="" className="text-gray-100 hover:bg-slate-100">Linkedin</a>
-          <a href="" className="text-gray-100 hover:bg-slate-100">Sobre</a>
+          <a href="" className="text-blue-600 hover:bg-slate-200">Meus projetos</a>
+          <a href="" className="text-blue-600 hover:bg-slate-100">Linkedin</a>
+          <a href="" className="text-blue-600 hover:bg-slate-100">Sobre</a>
 
         </div>
 
         <div className='ssm:block lg:hidden'>
           {toggle ? (
-            <AiOutlineContainer onClick={closeMenu} size={30} className='text-white' />
-          ) : (<HiMenuAlt1 onClick={openMenu} size={30} className='text-white' />)
+            <GoArrowRight onClick={closeMenu} size={30} />
+          ) : (<GoArrowLeft onClick={openMenu} size={30} />)
           }
         </div>
 
@@ -45,9 +45,9 @@ export default function Navbar() {
         ) : (
           <div className='flex justify-between ml-10'>
             <ul>
-              <li className='text-white text-x1 mb-2 cursor-pointer'>Meus projetos</li>
-              <li className='text-white text-x1 mb-2 cursor-pointer'>Linkedin</li>
-              <li className='text-white text-x1 mb-2 cursor-pointer' cursor-point>Sobre</li>
+              <li className='text-blue-600 mb-1 cursor-pointer'>Meus projetos</li>
+              <li className='text-blue-600 mb-1 cursor-pointer'>Linkedin</li>
+              <li className='text-blue-600 mb-1 cursor-pointer' cursor-point>Sobre</li>
             </ul>
           </div>
         )
